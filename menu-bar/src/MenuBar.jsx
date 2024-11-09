dfb44883099fa6648d283b6a58d6939cf0db14ef
AJMALAJU3 <ajmal.aju.340711@gmail.com>
2024-11-09 21:46:17 +0530

    navbar made responsive and animated

--- menu-bar/src/MenuBar.jsx
+++ menu-bar/src/MenuBar.jsx
@@ -2,49 +2,69 @@ import React, { useState } from 'react';
 import { FaHome, FaInfoCircle, FaBox, FaServicestack } from 'react-icons/fa';
 
 function MenuBar() {
-    const list = [
-        { name: "Home", icon: <FaHome size={24}/> },
-        { name: "About", icon: <FaInfoCircle size={20}/> },
-        { name: "Product", icon: <FaBox size={20}/> },
-        { name: "Service", icon: <FaServicestack size={24}/> },
-      ];
+  const list = [
+    { name: "Home", icon: <FaHome size={24}/> },
+    { name: "About", icon: <FaInfoCircle size={24}/> },
+    { name: "Product", icon: <FaBox size={24}/> },
+    { name: "Service", icon: <FaServicestack size={24}/> },
+  ];
+
   const [indexSelect, setIndexSelect] = useState(0);
 
   return (
-    <div className="w-full h-screen flex justify-center items-center  bg-gray-500">
-      <div className="flex text-white bg-black rounded-xl relative">
-        {
-          list.map((item, index) => (
-            <div className='flex items-center flex-col '>
+    <div className="w-full h-screen flex flex-col justify-end items-center bg-gray-500 md:p-1">
+      <div className="fixed w-full max-w-4xl bg-neutral-900 md:rounded-xl  flex justify-between items-center mx-3">
+        
+
+        <div className="relative flex-1 flex justify-between items-center z-10">
+          {list.map((item, index) => (
             <div
               key={index}
-              className={`w-32 text-center py-5 relative items-center flex flex-col justify-center z-10 cursor-pointer`}
+              className="flex-1 flex items-center justify-center text-white relative cursor-pointer"
               onClick={() => setIndexSelect(index)}
             >
-                <span className={`${indexSelect === index ? 'bottom-[1.8rem] ':'bottom-[0] '} duration-500 relative `}>{item.icon}</span>
-              
-              
+              <div
+                className={`flex flex-col items-center py-5 transition-all duration-300 ${
+                  indexSelect === index ? '' : 'text-white'
+                }`}
+              >
+
+                <span
+                  className={`relative ${indexSelect === index ? 'bottom-[1.8rem]' : 'bottom-0'} transition-all duration-500`}
+                >
+                  {item.icon}
+                </span>
+
+                <span
+                  className={`absolute  transition-all duration-500 ${
+                    indexSelect === index ? 'opacity-100 bottom-2' : 'opacity-0 bottom-0 '
+                  }`}
+                >
+                  {item.name}
+                </span>
+              </div>
             </div>
-            <span className={`${indexSelect === index ? 'bottom-[6px] opacity-100':'bottom-[0px] opacity-0'} duration-500 absolute`}>{item.name}</span>
-            </ div>
-          ))
-        }
+          ))}
+        </div>
+
+
         <div
-          className={`absolute z-0 top-[-2rem] rounded-full border-4 border-gray-500 ${
-            indexSelect === 1 ? "left-[10rem]"
-            : indexSelect === 2 ? " left-[18rem]"
-            : indexSelect === 3 ? "left-[26rem]"
-            : "left-[2rem]"
-          } text-center w-16 h-16 bg-gradient-to-tr from-black to-lime-500 duration-300 ease-linear text-transparent`}
+          className={`absolute bottom-0 transform transition-all duration-300 ease-linear z-0 flex justify-center`}
+          style={{
+            left: `${(indexSelect ) * 25}%`,  
+            width: '25%', 
+          }}
         >
-            <div className='  left-[-1.101rem] bg-gray-500 pt-[1px]  top-[1.7rem] absolute'>
-                <div className='w-full h-full bg-black rounded-tr-full p-2'>
+          <div className="w-16 h-16 bg-gradient-to-tr from-black to-lime-500 border-4 border-gray-500 rounded-full absolute bottom-8">
+          <div className='  left-[-1.101rem] bg-gray-500 pt-[1px]  top-[1.7rem] absolute'>
+                <div className='w-full h-full bg-neutral-900 rounded-tr-full p-2'>
                 </div>
             </div>
             <div className='  right-[-1.101rem] bg-gray-500 pt-[1px]  top-[1.7rem] absolute'>
-                <div className='w-full h-full bg-black rounded-tl-full p-2'>
+                <div className='w-full h-full bg-neutral-900 rounded-tl-full p-2'>
                 </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
